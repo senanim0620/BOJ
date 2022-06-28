e103f05e6a565a976dedb62a5b3d161bfa49bd69
senanim0620 <senanim0620@gmail.com>
2022-06-28 09:49:06 +0900

    .

---
+++ 220627/220627/2110.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long  arr[1000003];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
+
+
+// 진입차수를 구한다. => 인접행렬 버전
+int CountIndegree(int** graph, int vertex)
+{
+
+}
