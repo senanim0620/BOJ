dab2f1f8e228cf40be8dd537f31bd76d3adf2341
senanim0620 <senanim0620@gmail.com>
2022-06-29 09:45:26 +0900

    내 코드 반 남 코드 반 / 다시 해야함

--- 220628/220628/2667.cpp
+++ 220628/220628/2667.cpp
@@ -6,17 +6,62 @@
 
 using namespace std;
 
-vector<int> graph[27];
-bool arr[25][25] = { 0 };
+
+int num;
+
+int house_number = 0;
+int housenum[630] = { 0 };
+
+bool arr[25][25] = { 0 }; // 원본 저장
+bool buf[25][25] = { 0 }; // 탐색하면서 방문했던 적 있으면 1로 변환
+
+// 우 하 좌 상
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+//BFS
+
+void bfs(int y, int x)
+{
+	queue<pair<int, int>> q;
+	q.push(make_pair(y, x));
+
+	buf[y][x] = true;
+	housenum[house_number]++;
+
+	while (!q.empty())
+	{
+		y = q.front().first;
+		x = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (0 <= nx && nx < num && 0 <= ny && ny < num)
+			{
+				if (arr[ny][nx] == 1 && buf[ny][nx] == false)
+				{
+					buf[ny][nx] = true;
+					housenum[house_number]++;
+					q.push(make_pair(ny, nx));
+				}
+			}
+		}
+	}
+
+}
+
 
 
 int main()
 {
-	int num;
+	//int num;
 	cin >> num;
 
 	int size;
-
 
 	for (int i = 0; i < num; i++)
 	{
@@ -34,23 +79,34 @@ int main()
 			buf = size / count;
 			size = size % count;
 			arr[i][j] = buf;
-			if (buf == 1)
-			{
-				graph[i].push_back(j);
-				//graph[j].push_back(i);
-			}
-			else
-				;
 			count = count / 10;
 			j++;
 		}
-	} 
+	}
 	// 위에서 배열에 쇽쇽 넣음 // 잘들어감
 
-	BFS();
-	cout << "\n";
-	fibo(1);
 
-	//fibo(0);
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+		{
+			if (arr[i][j] == 1 && buf[i][j] == false)
+			{
+				house_number++;
+				bfs(i, j);
+			}
+
+		}
+	}
+
+	sort(housenum + 1, housenum + house_number + 1);
+
+	cout << house_number << "\n";
+
+	for (int i = 1; i <= house_number; i++)
+	{
+		cout << housenum[i] << "\n";
+	}
+	return 0;
 
 }
