340e46f27d1a3f2bcece093f7e3f5bcc92628617
senanim0620 <senanim0620@gmail.com>
2022-07-04 11:45:45 +0900

    너무 자연스럽게 음~ for문 써서 돌려돌려 하면 되겠네~ 싶었는데 시간 초과가 나서 더 효울적인 방법을 찾게 되었던 것

---
+++ 220704/220704/1712.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int A; // 고정비용
+	int B; // 가변비용, 즉 한대 생산에 드는 비용
+	int C; // 노트북가격
+
+	cin >> A >> B >> C;
+
+	int sena = C - B; // 순 수익
+	 
+	if (sena <=0) // 만드는 비용보다 싸게 팔면 손익분기점을 넘길 수 없다. 
+	{
+		cout << -1;
+		return 0;
+	}
+	else
+	{
+		int result = A / sena;
+		cout << result +1;
+		return 0;
+	}
+
+}
+
