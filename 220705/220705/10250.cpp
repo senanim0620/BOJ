040e900f4deac6f0d2ecb493c6db703a3147f906
senanim0620 <senanim0620@gmail.com>
2022-07-05 12:02:29 +0900

    10250끝

---
+++ 220705/220705/10250.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int testcase;
+	cin >> testcase;
+
+	int H, W, N;
+
+
+	for (int i = 0; i < testcase; i++)
+	{
+		cin >> H >> W >> N;
+		int floor = N % H ; 
+		int dong = (N / H);
+
+		if (floor == 0)
+			floor = H;
+		else
+			dong = dong + 1;
+
+		if (dong < 10)
+			cout << floor << "0" << dong<<"\n";
+		else
+			cout << floor << dong<<"\n";
+	}
+
+
+}
+
