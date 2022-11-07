34173c5922a6f1ed0d91c202f874956f20d86d87
senanim0620 <103409201+senanim0620@users.noreply.github.com>
2022-11-07 16:51:14 +0900

    [직접 작성] 위에서 아래로 C++

---
+++ 이코테/위에서아래로.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[500] = { 0 };
+
+int main(void)
+{
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> arr[i];
+	}
+
+	// 1. 선택 정렬
+
+	for (int i = 0; i < num; i++)
+	{
+		int min_index = i;
+		for (int j = i + 1; j < num; j++)
+		{
+			if (arr[min_index] < arr[j])
+				min_index = j;
+		}
+		swap(arr[i], arr[min_index]);
+	}
+
+	// 2. 삽입 정렬
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = i; j > 0; j--)
+			if (arr[j] < arr[j - 1])
+				swap(arr[j], arr[j - 1]);
+
+			else
+				break;
+	}
+
+	for (int i = 0; i < num; i++)
+		cout << arr[i] << ' ';
+
+
+
+}
