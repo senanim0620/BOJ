0aa8994dd1d35ef7e70477bfc63125b48159a018
senanim0620 <103409201+senanim0620@users.noreply.github.com>
2023-01-19 14:24:29 +0900

    Programmers
    초기세팅

---
+++ Programmers/Programmers/두 수의 곱.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+
+    answer = num1 / num2;
+    return answer;
+}
+
+int main()
+{
+    cout << solution(10, 5);
+}
+
